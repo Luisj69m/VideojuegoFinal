fbe006ef55aeb6ad26c462f7e7b9bf85d27bf89e
Luisj69m <luisjosemarcanofuentes@gmail.com>
2025-04-10 17:34:30 +0200

    Delete boladragon directory

--- boladragon/SCRIPT/creditos.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Recupera y aplica el volumen guardado en localStorage al cargar la página
-    const volumenGuardado = localStorage.getItem("volumen");
-    if (volumenGuardado !== null) 
-        aplicarVolumen(volumenGuardado);   
-});
